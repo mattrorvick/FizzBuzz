dd68f499e5d669d5015e763d250cbd51989e768d
mattrorvick <matthewrorvick@gmail.com>
2021-01-29 10:21:47 -0600

    init commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int input = scanner.nextInt();
+
+        if (input % 3 == 0 && input % 5 == 0)
+            System.out.println("FizzBuzz");
+        else if (input % 5 == 0)
+            System.out.println("Fizz");
+        else if (input % 3 == 0)
+            System.out.println("Buzz");
+        else
+            System.out.println(input);
+
+    }
+}
